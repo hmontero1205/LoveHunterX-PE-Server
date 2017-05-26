536fe1d9511e70acfcf592afa156be7b2676dd63
kevtehpro35 <kev.tehpro35@gmail.com>
2017-05-26 13:33:29 -0400

    Fixed spacing

--- LoveHunterX-PE-Server/src/Server.java
+++ LoveHunterX-PE-Server/src/Server.java
@@ -12,47 +12,47 @@ import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
 public class Server {
-	
+
 	private static final int PORT = 8080;
 	public static Database db;
-	
+
 	public void run() throws Exception {
 		EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
 
-        try {
-        	ServerBootstrap b = new ServerBootstrap();
-        	b.group(bossGroup, workerGroup)
-        	 .channel(NioServerSocketChannel.class)
-             .childHandler(new ChannelInitializer<SocketChannel>() {
-                 @Override
-                 public void initChannel(SocketChannel ch) throws Exception {
-                	 ChannelPipeline pipeline = ch.pipeline();
-                     pipeline.addLast("decoder", new StringDecoder());
-                     pipeline.addLast("encoder", new StringEncoder());
-                     pipeline.addLast(new Handler());
-                 }
-             })
-             .option(ChannelOption.SO_BACKLOG, 128)
-             .childOption(ChannelOption.SO_KEEPALIVE, true);
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+			 .channel(NioServerSocketChannel.class)
+			 .childHandler(new ChannelInitializer<SocketChannel>() {
+				@Override
+					public void initChannel(SocketChannel ch) throws Exception {
+						ChannelPipeline pipeline = ch.pipeline();
+						pipeline.addLast("decoder", new StringDecoder());
+						pipeline.addLast("encoder", new StringEncoder());
+						pipeline.addLast(new Handler());
+					}
+				})
+			 .option(ChannelOption.SO_BACKLOG, 128)
+			 .childOption(ChannelOption.SO_KEEPALIVE, true);
 
-        	ChannelFuture f = b.bind(PORT).sync();
-        	f.channel().closeFuture().sync();
-        } finally {
-        	workerGroup.shutdownGracefully();
-        	bossGroup.shutdownGracefully();
-        }
+			ChannelFuture f = b.bind(PORT).sync();
+			f.channel().closeFuture().sync();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("Running");
 		db = new Database();
-		
+
 		try {
 			new Server().run();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
+
 }
